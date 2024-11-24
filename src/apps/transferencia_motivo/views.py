cd5b8139bd80b121d432d721a5d4537728174f5b
Facundo Sosa López <fssl2004@gmail.com>
2024-11-24 13:57:55 -0300

    Reset password.

--- src/apps/transferencia_motivo/views.py
+++ src/apps/transferencia_motivo/views.py
@@ -4,28 +4,28 @@ from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import MotivoTransferencia
 from .forms import MotivoTransferenciaForm
 from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib import messages
+from config.mixins import TestStaffMixin
 
-class MotivoListView(ListView):
+class MotivoListView(TestStaffMixin, ListView):
     model = MotivoTransferencia
     template_name = 'transferencia_motivo/motivo_list.html'
     context_object_name = 'motivos'
 
-class MotivoCreateView(SuccessMessageMixin, CreateView):
+class MotivoCreateView(TestStaffMixin,SuccessMessageMixin, CreateView):
     model = MotivoTransferencia
     form_class = MotivoTransferenciaForm
     template_name = 'transferencia_motivo/motivo_form.html'
     success_url = reverse_lazy('transferencia_motivo:list')
     success_message = "Guardado con éxito."
 
-class MotivoUpdateView(SuccessMessageMixin, UpdateView):
+class MotivoUpdateView(TestStaffMixin, SuccessMessageMixin, UpdateView):
     model = MotivoTransferencia
     fields = ['descripcion']
     template_name = 'transferencia_motivo/motivo_form.html'
     success_url = reverse_lazy('transferencia_motivo:list')
     success_message = "Guardado con éxito."
 
-class MotivoDeleteView(SuccessMessageMixin, DeleteView):
+class MotivoDeleteView(TestStaffMixin,SuccessMessageMixin, DeleteView):
     model = MotivoTransferencia
     template_name = 'transferencia_motivo/motivo_confirm_delete.html'
     success_url = reverse_lazy('transferencia_motivo:list')
