35fbd530933e5c2d4a81157bc80d68d30681d1f4
Facundo Sosa López <fssl2004@gmail.com>
2024-11-24 22:35:40 -0300

    Asguramiento de rutas. Paginator para que funcione con filtros

--- src/apps/movimientos/urls.py
+++ src/apps/movimientos/urls.py
@@ -5,7 +5,7 @@ from .views import (
    IngresoDinero,
    TransferenciaCuenta,
    MovimientoDetailView,
-   HistorialMovimientosAdmin
+   # HistorialMovimientosAdmin
 )
 
 app_name = "movimientos"
@@ -16,5 +16,5 @@ urlpatterns = [
     path("ingreso_dinero/", IngresoDinero.as_view(), name="ingreso_dinero"),
     path('transferencia_cuenta/<int:cuenta_asociada_id>/', TransferenciaCuenta.as_view(), name='transferencia_cuenta'),
     path('movimiento_detail/<int:pk>/', MovimientoDetailView.as_view(), name='movimiento_detail'),
-    path("admin_historial/", HistorialMovimientosAdmin.as_view(), name="admin_historial"),
+    # path("admin_historial/", HistorialMovimientosAdmin.as_view(), name="admin_historial"),
 ]
